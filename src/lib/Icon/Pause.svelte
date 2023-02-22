313023bf7e86d809a72964d916072d397aa511d8
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-02-22 03:16:58 -0300

    fix: Set correct viewBox width to Pause icon component

--- src/lib/Icon/Pause.svelte
+++ src/lib/Icon/Pause.svelte
@@ -6,7 +6,7 @@
   xmlns="http://www.w3.org/2000/svg"
   width="auto"
   height={size}
-  viewBox="0 0 320 512"
+  viewBox="0 0 448 512"
 >
   <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
   <path
